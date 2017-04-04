fd7267f0ce93103a8ad4fbc650eba63b6d8ac247
ranajoy-dutta <ranajoydutta7@gmail.com>
2017-04-04 13:30:18 +0530

    Pyramid Pattern using star(*)

---
+++ Pattern_3.py
@@ -0,0 +1,18 @@
+''' PROGRAM TO PRINT THE FOLLOWING PATTERN
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *              '''
+
+#--- main ---
+k = 0
+rows=int(input("HOW MANY STAR(*) LINES SHOULD BE PRINTED? :: "))
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+    while k != (2*i-1):
+        print("* ", end="")
+        k = k + 1
+    k = 0
+    print()
